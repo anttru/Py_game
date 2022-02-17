77e19ce2beaa49184eed7e9bcc1a65bfb5abc0d6
Antonio Trujillo <antrusant@gmail.com>
2022-02-17 20:06:37 +0100

    first commit

---
+++ main_loop.py
@@ -0,0 +1,44 @@
+from re import A
+import pygame as pg
+
+pg.init()
+
+mainscreen = pg.display.set_mode((600,800))
+
+game_over = False
+x = 300
+y = 400
+xinc = 0.25
+yinc = 0.25
+ball = pg.draw.circle(mainscreen, (255,255,0), (x,y), 10)
+while not game_over:
+    #procesar eventos
+    events = pg.event.get()
+    for event in events:
+        if event.type == pg.QUIT:
+            game_over = True
+        
+    
+    #modificar objetos
+    if ball.center[0] > 595 or ball.center[0] < 5:
+        xinc *= -1
+    if ball.center[1] > 795 or ball.center[1] < 5: 
+        yinc *= -1
+    x += xinc
+    y += yinc
+    #refrescar pantalla
+    mainscreen.fill((255,0,0))
+    ball = pg.draw.circle(mainscreen, (255,255,0), (x,y), 10)
+    pg.display.flip()
+    
+
+pg.quit()
+
+ 
+ 
+
+         
+
+
+
+
